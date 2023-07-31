41d5cf750f141ed069e3ecb6c2ca83b1c0f69640
rjthapaa <127729631+rjthapaa@users.noreply.github.com>
2023-07-31 14:43:31 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Batch 3!",
+        message: "Hello From Batch 2+1!",
       },
       null,
       2
